753a5f35eb5de4ceba8a4ac76d9cac8d53652ffa
juhaodong <Jhd961213>
2020-06-19 17:02:18 +0200

    增加自动配置更新

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.aaden.app.vue',
+        productName: 'Aaden-Vue',
+        copyright: 'Copyright © 2019', // 版权信息
+        linux: {
+          target: 'snap'
+        },
+        publish: 'always',
+        snap: {
+          autoStart: true,
+          grade: 'devel',
+          publish: [
+            {
+              provider: 'snapStore',
+              channels: 'beta'
+            }
+          ]
+        }
+      }
+    },
     i18n: {
       locale: 'zh',
       fallbackLocale: 'de',
