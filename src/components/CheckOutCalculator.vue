f519242e6b9e24743c8495dd70aa1832c101c22a
Haodong Ju <juhaodong@gmail.com>
2020-11-04 16:40:21 +0100

    增加了对于多支付方式的支持

--- src/components/CheckOutCalculator.vue
+++ src/components/CheckOutCalculator.vue
@@ -1,172 +1,193 @@
 <template>
-    <div class="d-flex fill-height">
-        <div style="width: 480px;max-height: 700px;height: 100%" class="calculator pa-2 d-flex flex-column fill-height">
-            <v-card style="width: 100%" class="display pa-4 d-flex
+  <div class="d-flex fill-height">
+    <div style="width: 480px;max-height: 700px;height: 100%" class="calculator pa-2 d-flex flex-column fill-height">
+      <v-card style="width: 100%" class="display pa-4 d-flex
                  justify-space-between align-end">
-                <div>
-                    <div class="totalBlock">
-                        <div>
-                            <v-chip dark label color="primary" small>
-                                {{$t("应付总额")}}
-                            </v-chip>
-                        </div>
-                        <span class="totalNumber">
-                           {{total|priceDisplay}}
-                        </span>
-                    </div>
-                    <div class="totalBlock">
-                        <div>
-                            <v-chip dark label class="font-weight-bold" color="error" small>
-                                {{$t("剩余总额")}}
-                            </v-chip>
-                        </div>
-                        <span class="totalNumber">
-                           {{remainTotal|priceDisplay}}
-                        </span>
-                    </div>
-                </div>
-                <div class="totalBlock">
-                    <div>
-                        <v-chip dark label color="success">
-                            {{$t("将要支付")}}
-                        </v-chip>
-                    </div>
-                    <div style="width:240px;height: 64px">
-                        <v-text-field height="64px" reverse solo
-                                      class="payingNumber py-1 text-right"
-                                      v-model="inputBuffer"
-                                      :placeholder="''+remainTotal.toFixed(2)"/>
-                    </div>
-                </div>
-            </v-card>
-            <v-sheet elevation="2" class="keyboard flex-grow-1 pa-4">
-<!--                keyBoard-->
-                <template v-for="i in keyArr.flat()">
-                    <v-btn :ripple="false" v-if="i!=='mdi-dots-horizontal'" @click="input(i)" block x-large
-                           class="key"
-                           style="height: 100%"
-                           :elevation="2"
-                           :key="'key'+i">
-                        <v-icon v-if="!isNaN(i)" x-large>mdi-numeric-{{i}}</v-icon>
-                        <v-icon x-large v-else>{{i}}</v-icon>
-                    </v-btn>
-                    <v-menu offset-x :key="'key'+i" v-else>
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-btn @click="input(i)" block x-large class="key"
-                                   style="height: 100%"
-                                   :elevation="2"
-                                   v-bind="attrs"
-                                   v-on="on"
-                                   :key="'key'+i">
-                                <v-icon x-large>{{i}}</v-icon>
-                            </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item
-                                    @click="input(item)"
-                                    v-for="(item, index) in extraPaymentMethod"
-                                    :key="index"
-                            >
-                                <v-list-item-icon>
-                                    <v-icon>{{ item }}</v-icon>
-                                </v-list-item-icon>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        {{extraPaymentMethodName[index]}}
-                                    </v-list-item-title>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                </template>
-            </v-sheet>
-        </div>
-        <div style="width: calc(100vw - 200px - 480px)" class="paymentLog pa-2">
-            <div class="my-3">
-                <h3>{{$t("结账记录")}}</h3>
-            </div>
-            <div class="my-3" v-dragscroll style="max-height: calc(100vh - 560px);overflow:hidden">
-                <template v-for="(paymentInfo,index) in paymentLog">
-                    <v-sheet :key="'price'+paymentInfo.hash" :elevation="0"
-                             style="width: 100%" class="d-flex justify-space-between pa-2 my-1">
-                        <h2 class="font-weight-bold"
-                            style="font-size: 24px">
-                            {{ paymentInfo.price|priceDisplay }}
-                        </h2>
-                        <div>
-                            <v-btn text color="success">
-                                <v-icon large>{{paymentInfo.icon}}</v-icon>
-                            </v-btn>
-                            <v-btn @click="withdrawPayment(index)" icon color="error">
-                                <v-icon>mdi-close</v-icon>
-                            </v-btn>
-                        </div>
-                    </v-sheet>
-                    <v-divider :key="'d'+paymentInfo.hash"></v-divider>
-                </template>
-            </div>
+        <div>
+          <div class="totalBlock">
             <div>
-                <v-sheet class="my-6">
-                    <h4>
-                        {{$t('tableCheckOutBillTypeLabel')}}
-                    </h4>
-                    <v-sheet class="my-2" style="background: transparent">
-                        <v-chip-group
-                                v-model="billType"
-                                mandatory column
-                                active-class="primary--text">
-                            <v-chip x-large label>
-                                {{$t('tableCheckOutBillTypeOptionNormal')}}
-                            </v-chip>
-                            <v-chip x-large label>
-                                {{$t('tableCheckOutBillTypeOptionCompany')}}
-                            </v-chip>
-                            <v-chip x-large label>
-                                {{$t('tableCheckOutBillTypeOption3')}}
-                            </v-chip>
-                        </v-chip-group>
-                    </v-sheet>
-                </v-sheet>
-                <v-divider class="my-3"></v-divider>
-                <div style="height: 120px;display: grid;grid-template-columns: 1fr 1fr">
-                    <div class="pa-2">
-                        <v-btn tile fab
-                               outlined
-                               @click="cancel"
-                               color="error" block x-large>
-                            {{$t('cancel')}}
-                        </v-btn>
-                    </div>
-                    <div class="pa-2">
-                        <v-btn color="success"
-                               @click="checkOut"
-                               elevation="0"
-                               :disabled="!equals(remainTotal,0)||paymentLog.length===0"
-                               tile fab block x-large> {{$t('tableCheckOutConfirm')}}
-                        </v-btn>
-                    </div>
-                </div>
-                <div style="height: 120px">
-                  <h4>{{$t('或者使用0小费,现金,普通账单进行')}}</h4>
-                  <div class="pa-2">
-                    <v-btn color="primary"
-                           @click="checkOut"
-                           elevation="0"
-                           :disabled="paymentLog.length!==0"
-                           tile fab block x-large> {{$t('QuickBill')}}
-                    </v-btn>
-                  </div>
-                </div>
+              <v-chip dark label color="primary" small>
+                {{ $t('应付总额') }}
+              </v-chip>
             </div>
+            <span class="totalNumber">
+                           {{ total|priceDisplay }}
+                        </span>
+          </div>
+          <div class="totalBlock">
+            <div>
+              <v-chip dark label class="font-weight-bold" color="error" small>
+                {{ $t('剩余总额') }}
+              </v-chip>
+            </div>
+            <span class="totalNumber">
+                           {{ remainTotal|priceDisplay }}
+                        </span>
+          </div>
         </div>
+        <div class="totalBlock">
+          <div>
+            <v-chip dark label color="success">
+              {{ $t('将要支付') }}
+            </v-chip>
+          </div>
+          <div style="width:240px;height: 64px">
+            <v-text-field height="64px" reverse solo
+                          class="payingNumber py-1 text-right"
+                          v-model="inputBuffer"
+                          :placeholder="''+remainTotal.toFixed(2)"/>
+          </div>
+        </div>
+      </v-card>
+      <v-sheet elevation="2" class="keyboard flex-grow-1 pa-4">
+        <!--                keyBoard-->
+        <template v-for="i in keyArr.flat()">
+          <v-btn :ripple="false" v-if="i!=='mdi-dots-horizontal'" @click="input(i)" block x-large
+                 class="key"
+                 style="height: 100%"
+                 :elevation="2"
+                 :key="'key'+i">
+            <v-icon v-if="!isNaN(i)" x-large>mdi-numeric-{{ i }}</v-icon>
+            <v-icon x-large v-else>{{ i }}</v-icon>
+          </v-btn>
+          <v-menu offset-x :key="'key'+i" v-else>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn @click="input(i)" block x-large class="key"
+                     style="height: 100%"
+                     :elevation="2"
+                     v-bind="attrs"
+                     v-on="on"
+                     :key="'key'+i">
+                <v-icon x-large>{{ i }}</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                  @click="input(item)"
+                  v-for="(item, index) in realExtraPaymentMethodName"
+                  :key="index"
+              >
+                <v-list-item-icon>
+                  <v-icon>{{ realExtraPaymentMethod[index] }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+      </v-sheet>
     </div>
+    <div style="width: calc(100vw - 200px - 480px)" class="paymentLog pa-2">
+      <div class="my-3">
+        <h3>{{ $t('结账记录') }}</h3>
+      </div>
+      <div class="my-3" v-dragscroll style="max-height: calc(100vh - 560px);overflow:hidden">
+        <template v-for="(paymentInfo,index) in paymentLog">
+          <v-sheet :key="'price'+paymentInfo.hash" :elevation="0"
+                   style="width: 100%" class="d-flex justify-space-between pa-2 my-1">
+            <h2 class="font-weight-bold"
+                style="font-size: 24px">
+              {{ paymentInfo.price|priceDisplay }}
+            </h2>
+            <div>
+              <v-btn text color="success">
+                <template v-if="paymentInfo.icon.startsWith('mdi')">
+                  <v-icon large>{{ paymentInfo.icon }}</v-icon>
+                </template>
+                <template v-else>
+                  {{paymentInfo.icon}}
+                </template>
+
+              </v-btn>
+              <v-btn @click="withdrawPayment(index)" icon color="error">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </div>
+          </v-sheet>
+          <v-divider :key="'d'+paymentInfo.hash"></v-divider>
+        </template>
+      </div>
+      <div>
+        <v-sheet class="my-6">
+          <h4>
+            {{ $t('tableCheckOutBillTypeLabel') }}
+          </h4>
+          <v-sheet class="my-2" style="background: transparent">
+            <v-chip-group
+                v-model="billType"
+                mandatory column
+                active-class="primary--text">
+              <v-chip x-large label>
+                {{ $t('tableCheckOutBillTypeOptionNormal') }}
+              </v-chip>
+              <v-chip x-large label>
+                {{ $t('tableCheckOutBillTypeOptionCompany') }}
+              </v-chip>
+              <v-chip x-large label>
+                {{ $t('tableCheckOutBillTypeOption3') }}
+              </v-chip>
+            </v-chip-group>
+          </v-sheet>
+        </v-sheet>
+        <v-divider class="my-3"></v-divider>
+        <div style="height: 120px;display: grid;grid-template-columns: 1fr 1fr">
+          <div class="pa-2">
+            <v-btn tile fab
+                   outlined
+                   @click="cancel"
+                   color="error" block x-large>
+              {{ $t('cancel') }}
+            </v-btn>
+          </div>
+          <div class="pa-2">
+            <v-btn color="success"
+                   @click="checkOut"
+                   elevation="0"
+                   :disabled="!equals(remainTotal,0)||paymentLog.length===0"
+                   tile fab block x-large> {{ $t('tableCheckOutConfirm') }}
+            </v-btn>
+          </div>
+        </div>
+        <div style="height: 120px">
+          <h4>{{ $t('或者使用0小费,现金,普通账单进行') }}</h4>
+          <div class="pa-2">
+            <v-btn color="primary"
+                   @click="checkOut"
+                   elevation="0"
+                   :disabled="paymentLog.length!==0"
+                   tile fab block x-large> {{ $t('QuickBill') }}
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 import { dragscroll } from 'vue-dragscroll'
-import { fastSweetAlertRequest } from '../oldjs/common'
+import { fastSweetAlertRequest } from '@/oldjs/common'
+import Payment from 'aaden-base-model/lib/Models/Payment'
 
+const includedPaymentMethods = [0, 1, 2, 9, 4, 10]
+const defaultRealName = {
+  'mdi-minus': 'reverse',
+  'mdi-backspace': 'back',
+  'mdi-restart': 'clear',
+  'mdi-circle-small': '.',
+  'mdi-cash-usd': '1',
+  'mdi-bell': '9',
+  'mdi-credit-card-outline': '2',
+  KreditCard: '3',
+  Gutschein: '4',
+  SumUp: '10',
+  'mdi-dots-horizontal': 'more'
+}
 export default {
   name: 'CheckOutCalculator',
   props: {
@@ -180,22 +201,13 @@ export default {
   data: function () {
     return {
       billType: 0,
-      realName: {
-        'mdi-minus': 'reverse',
-        'mdi-backspace': 'back',
-        'mdi-restart': 'clear',
-        'mdi-circle-small': '.',
-        'mdi-cash-usd': '1',
-        'mdi-bell': '9',
-        'mdi-cards': '10',
-        'mdi-credit-card-outline': '2',
-        'mdi-credit-card-wireless': '3',
-        'mdi-card-account-details': '4',
-        'mdi-dots-horizontal': 'more'
-      },
+      paymentMethods: [],
+      realName: defaultRealName,
       inputBuffer: '',
-      extraPaymentMethod: ['mdi-credit-card-wireless', 'mdi-card-account-details', 'mdi-cards'],
-      extraPaymentMethodName: ['KreditCard', 'Gutschein', 'SumUp'],
+      extraPaymentMethod: [
+        'mdi-card-account-details',
+        'mdi-cards'],
+      extraPaymentMethodName: ['Gutschein', 'SumUp'],
       paymentLog: []
     }
   },
@@ -215,6 +227,17 @@ export default {
           ['mdi-dots-horizontal', 0, 'mdi-circle-small', 'mdi-cash-usd']]
       }
     },
+    realExtraPaymentMethod: function () {
+      const res = this.extraPaymentMethod
+      res.push(...this.paymentMethods.map(p => p.icon || p.id))
+      console.log(res)
+      return res
+    },
+    realExtraPaymentMethodName: function () {
+      const res = this.extraPaymentMethodName
+      res.push(...this.paymentMethods.map(p => p.name))
+      return res
+    },
     remainTotal: function () {
       const logTotal = this.paymentLog.reduce((cry, i) => {
         cry += parseFloat(i.price)
@@ -223,7 +246,18 @@ export default {
       return this.total - logTotal
     }
   },
+  created () {
+    this.loadPaymentMethods()
+  },
   methods: {
+    async loadPaymentMethods () {
+      this.paymentMethods = (await Payment.getList())
+        .filter(p => !includedPaymentMethods.includes(p.id))
+      this.realName = Object.assign({}, defaultRealName)
+      this.paymentMethods.forEach(p => {
+        this.$set(this.realName, p.name, p.id)
+      })
+    },
     equals (a, b) {
       return Math.abs(a - b) < 0.001
     },
@@ -303,13 +337,13 @@ export default {
             break
           case '10':
             window.open('sumupmerchant://pay/1.0?' +
-              'affiliate-key=7ca84f17-84a5-4140-8df6-6ebeed8540fc&' +
-              'app-id=com.example.myapp&' +
-              'total=' + this.readBuffer() + '&currency=EUR' +
-              '&title=Taxi Ride' +
-              '&receipt-mobilephone=+3531234567890' +
-              '&receipt-email=customer@mail.com' +
-              '&callback=https://aaden.io')
+                'affiliate-key=7ca84f17-84a5-4140-8df6-6ebeed8540fc&' +
+                'app-id=com.example.myapp&' +
+                'total=' + this.readBuffer() + '&currency=EUR' +
+                '&title=Taxi Ride' +
+                '&receipt-mobilephone=+3531234567890' +
+                '&receipt-email=customer@mail.com' +
+                '&callback=https://aaden.io')
             this.logPayment(c)
             break
           default:
@@ -323,30 +357,30 @@ export default {
 
 <style scoped>
 
-    .totalNumber {
-        width: fit-content;
-        font-size: 24px;
-    }
+.totalNumber {
+  width: fit-content;
+  font-size: 24px;
+}
 
-    .payingNumber {
-        width: fit-content;
-        font-size: 42px;
-    }
+.payingNumber {
+  width: fit-content;
+  font-size: 42px;
+}
 
-    .keyboard {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        grid-template-rows: 1fr 1fr 1fr 1fr;
-        grid-gap: 4px;
-    }
+.keyboard {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-gap: 4px;
+}
 
-    .key {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 48px;
-        height: auto;
-        border-radius: 5px;
-    }
+.key {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 48px;
+  height: auto;
+  border-radius: 5px;
+}
 
 </style>
