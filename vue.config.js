d611abb2f3cdd1b5ac2ec1ec8e8ff7e4c2f8b1f6
FalconRabbit233 <34422044+FalconRabbit233@users.noreply.github.com>
2021-03-26 09:30:05 +0100

    windows部署所用配置备忘

--- vue.config.js
+++ vue.config.js
@@ -5,22 +5,8 @@ module.exports = {
         appId: 'com.aaden.app.vue',
         productName: 'Aaden-Vue',
         copyright: 'Copyright InnerKen GmbH 2019', // 版权信息
-        linux: {
-          target: 'snap'
-        },
         win: {
           target: 'nsis'
-        },
-        publish: 'always',
-        snap: {
-          autoStart: true,
-          grade: 'stable',
-          publish: [
-            {
-              provider: 'snapStore',
-              channels: 'candidate'
-            }
-          ]
         }
       },
       nodeIntegration: true
