c3126a5f569f5c5b82ebc01151d27811b2c75aed
juhaodong <juhaodong@gmail.com>
2021-06-20 00:13:17 +0200

    提高性能,同时不显著降低性能

--- src/views/TablePage.vue
+++ src/views/TablePage.vue
@@ -32,12 +32,12 @@
                       class="white">
                 <keep-alive>
                   <dish-card-list
-                    :dish-list-model="orderListModel"
-                    :discount-ratio="discountRatio"
-                    :default-expand="cartListModel.list.length===0"
-                    :click-callback="addToSplit"
-                    extra-height="96px"
-                    :title="$t('haveOrderedDish')"
+                      :dish-list-model="orderListModel"
+                      :discount-ratio="discountRatio"
+                      :default-expand="cartListModel.list.length===0"
+                      :click-callback="addToSplit"
+                      extra-height="96px"
+                      :title="$t('haveOrderedDish')"
                   />
                 </keep-alive>
               </v-card>
@@ -46,17 +46,17 @@
                       v-show="cartListModel.list.length>0"
                       class="white">
                 <dish-card-list
-                  ref="cartList"
-                  @current-dish-change="cartCurrentDish=$event"
-                  :reset-current-expand-index="true"
-                  :show-number="true"
-                  :extra-height="'196px'"
-                  :color="'#707070'"
-                  :dish-list-model="cartListModel"
-                  :show-edit="true"
-                  :click-callback="removeDish"
-                  :title="$t('新增菜品')"
-                  :default-expand="true"/>
+                    ref="cartList"
+                    @current-dish-change="cartCurrentDish=$event"
+                    :reset-current-expand-index="true"
+                    :show-number="true"
+                    :extra-height="'196px'"
+                    :color="'#707070'"
+                    :dish-list-model="cartListModel"
+                    :show-edit="true"
+                    :click-callback="removeDish"
+                    :title="$t('新增菜品')"
+                    :default-expand="true"/>
                 <v-toolbar dense>
                   <v-toolbar-items class="flex-grow-1 mx-n3">
                     <v-btn @click="cartListModel.clear()" class="mr-1" color="error">
@@ -120,7 +120,7 @@
                       <div @click="changeCategory(category.id,toggle)" class="menu-item"
                            :class="active?'active elevation-4':''"
                            :style="{backgroundColor:category.color, color:getColorLightness(category.color)>128?'#000':'#fff'}">
-                       {{ category.name }}
+                        {{ category.name }}
                       </div>
                     </v-item>
                   </template>
@@ -141,19 +141,19 @@
                 </div>
                 <template v-for="dish of filteredDish">
                   <dish-block
-                    v-ripple
-                    :key="'dish'+dish.code"
-                    :code="dish.code"
-                    :count="dish.count"
-                    :display-color="dish.displayColor"
-                    :dish-name="dish.dishName"
-                    :foreground="dish.foreground"
-                    :font-size="Config.dishBlockFontSize"
-                    :have-mod="dish.haveMod"
-                    :is-free="dish.isFree"
-                    :price="dish.price"
-                    @click-tune="showModification(dish,1)"
-                    @click="orderOneDish(dish.code)"/>
+                      v-ripple
+                      :key="'dish'+dish.code"
+                      :code="dish.code"
+                      :count="dish.count"
+                      :display-color="dish.displayColor"
+                      :dish-name="dish.dishName"
+                      :foreground="dish.foreground"
+                      :font-size="Config.dishBlockFontSize"
+                      :have-mod="dish.haveMod"
+                      :is-free="dish.isFree"
+                      :price="dish.price"
+                      @click-tune="showModification(dish,1)"
+                      @click="orderOneDish(dish.code)"/>
                 </template>
               </div>
             </v-card>
@@ -186,129 +186,129 @@
           >
             <div style="display: grid;grid-template-columns: repeat(3,1fr);grid-gap: 4px">
               <grid-button
-                :loading="isSendingRequest"
-                icon="mdi-arrow-left"
-                :text="$t('Home')"
-                @click="back"
+                  :loading="isSendingRequest"
+                  icon="mdi-arrow-left"
+                  :text="$t('Home')"
+                  @click="back"
               />
 
               <grid-button
-                :loading="isSendingRequest"
-                icon="mdi-printer"
-                color="#fec945"
-                :text="$t('重新打印')"
-                @click="reprintOrder"
+                  :loading="isSendingRequest"
+                  icon="mdi-printer"
+                  color="#fec945"
+                  :text="$t('重新打印')"
+                  @click="reprintOrder"
               />
               <grid-button
-                :loading="isSendingRequest"
-                icon="mdi-printer-pos"
-                text="Zwischen"
-                color="#24b646"
-                @click="zwitchenBon"
+                  :loading="isSendingRequest"
+                  icon="mdi-printer-pos"
+                  text="Zwischen"
+                  color="#24b646"
+                  @click="zwitchenBon"
               />
               <grid-button
-                :loading="isSendingRequest"
-                icon="mdi-swap-horizontal"
-                color="#ff8c50"
-                :text=" $t('tableChange') "
-                @click="popAuthorize('',
+                  :loading="isSendingRequest"
+                  icon="mdi-swap-horizontal"
+                  color="#ff8c50"
+                  :text=" $t('tableChange') "
+                  @click="popAuthorize('',
                 () => popChangeTablePanel(tableDetailInfo.tableBasicInfo.name ))"
               />
               <grid-button
-                :loading="isSendingRequest"
-                color="#272727"
-                icon="mdi-merge"
-                :text="$t('tableMerge')"
-                @click="popAuthorize('',
+                  :loading="isSendingRequest"
+                  color="#272727"
+                  icon="mdi-merge"
+                  :text="$t('tableMerge')"
+                  @click="popAuthorize('',
                 () => popMergeTablePanel(tableDetailInfo.tableBasicInfo.name ))"
               />
               <grid-button
-                :loading="isSendingRequest"
-                icon="mdi-account"
-                :text="$t('Übergabe')"
-                color="#3f49dd"
-                @click="changeServant"
+                  :loading="isSendingRequest"
+                  icon="mdi-account"
+                  :text="$t('Übergabe')"
+                  color="#3f49dd"
+                  @click="changeServant"
               />
               <grid-button
-                :loading="isSendingRequest"
-                v-if="consumeTypeId===2"
-                icon="mdi-map"
-                text="Addr. "
-                color="indigo"
-                @click="addressFormOpen=true"
+                  :loading="isSendingRequest"
+                  v-if="consumeTypeId===2"
+                  icon="mdi-map"
+                  text="Addr. "
+                  color="indigo"
+                  @click="addressFormOpen=true"
               />
               <template v-else-if="consumeTypeStatusId<2">
                 <grid-button
-                  :loading="isSendingRequest"
-                  icon="mdi-check"
-                  text="Akzept."
-                  color="success"
-                  @click="acceptOrder"
+                    :loading="isSendingRequest"
+                    icon="mdi-check"
+                    text="Akzept."
+                    color="success"
+                    @click="acceptOrder"
                 />
                 <grid-button
-                  :loading="isSendingRequest"
-                  icon="mdi-close"
-                  text="Ablehnen"
-                  color="error"
-                  @click="rejectOrder"
+                    :loading="isSendingRequest"
+                    icon="mdi-close"
+                    text="Ablehnen"
+                    color="error"
+                    @click="rejectOrder"
                 />
               </template>
 
             </div>
 
             <address-display
-              :should-open-menu.sync="addressFormOpen"
-              @address-change="submitRawAddressInfo"
-              v-if="consumeTypeId===2"
-              @accept="acceptOrderWithTime"
-              @reject="rejectOrder"
-              :consume-type-status-id="consumeTypeStatusId"
-              :raw-address-info="realAddressInfo"/>
+                :should-open-menu.sync="addressFormOpen"
+                @address-change="submitRawAddressInfo"
+                v-if="consumeTypeId===2"
+                @accept="acceptOrderWithTime"
+                @reject="rejectOrder"
+                :consume-type-status-id="consumeTypeStatusId"
+                :raw-address-info="realAddressInfo"/>
           </div>
 
           <div v-else style="display: grid;grid-template-columns: repeat(3,1fr);grid-gap: 4px" class="pa-2">
             <grid-button
-              @click="cartListModel.clear()"
-              icon="mdi-delete-sweep"
-              text="Leeren"
-              color="error"
+                @click="cartListModel.clear()"
+                icon="mdi-delete-sweep"
+                text="Leeren"
+                color="error"
             ></grid-button>
             <grid-button
-              @click="orderDish(cartListModel.list,false)"
-              :loading="isSendingRequest"
-              icon="mdi-printer-off"
-              text="Bestellen"
-              color="#000"
+                @click="orderDish(cartListModel.list,false)"
+                :loading="isSendingRequest"
+                icon="mdi-printer-off"
+                text="Bestellen"
+                color="#000"
             ></grid-button>
             <grid-button
-              :loading="isSendingRequest"
-              icon="mdi-printer"
-              text="Drucken"
-              @click="orderDish(cartListModel.list)"
+                :loading="isSendingRequest"
+                icon="mdi-printer"
+                text="Drucken"
+                @click="orderDish(cartListModel.list)"
             ></grid-button>
             <template v-if="cartCurrentDish">
               <grid-button
-                @click="cartCurrentDish.change(-1)"
-                icon="mdi-minus"
-                color="error"
+                  @click="cartCurrentDish.change(-1)"
+                  icon="mdi-minus"
+                  color="error"
               ></grid-button>
               <grid-button
-                :disabled="cartCurrentDish.haveMod<1"
-                @click="cartCurrentDish.edit();cartCurrentDish.change(-1)"
-                icon="mdi-tune"
-                color="warning"
-                text="ohne/mit"
+                  :disabled="cartCurrentDish.haveMod<1"
+                  @click="cartCurrentDish.edit();cartCurrentDish.change(-1)"
+                  icon="mdi-tune"
+                  color="warning"
+                  text="ohne/mit"
               ></grid-button>
               <grid-button
-                @click="cartCurrentDish.change(1)"
-                icon="mdi-plus"
-                color="success"
+                  @click="cartCurrentDish.change(1)"
+                  icon="mdi-plus"
+                  color="success"
               ></grid-button>
               <grid-button
-                @click="editNote"
-                icon="mdi-notebook-edit"
-                color="#666666"
-                text="Notiz"
+                  @click="editNote"
+                  icon="mdi-notebook-edit"
+                  color="#666666"
+                  text="Notiz"
               ></grid-button>
             </template>
 
@@ -316,13 +316,13 @@
           <v-spacer></v-spacer>
           <div>
             <v-text-field
-              class="ma-2"
-              hide-details
-              clearable
-              style="font-size: 36px"
-              ref="ins"
-              @input="input=displayInput"
-              v-model="displayInput"
+                class="ma-2"
+                hide-details
+                clearable
+                style="font-size: 36px"
+                ref="ins"
+                @input="input=displayInput"
+                v-model="displayInput"
             />
             <keyboard @input="numberInput"
                       :keys="keyboardLayout"></keyboard>
@@ -370,13 +370,13 @@ left: 304px"
               </v-btn>
             </div>
             <dish-card-list
-              class="flex-grow-1"
-              extra-height="48px"
-              :discount-ratio="discountRatio"
-              :default-expand="true"
-              :dish-list-model="splitOrderListModel"
-              :click-callback="removeFromSplitOrder"
-              :title="$t('operation')"/>
+                class="flex-grow-1"
+                extra-height="48px"
+                :discount-ratio="discountRatio"
+                :default-expand="true"
+                :dish-list-model="splitOrderListModel"
+                :click-callback="removeFromSplitOrder"
+                :title="$t('operation')"/>
           </div>
         </div>
       </template>
@@ -395,30 +395,30 @@ left: 304px"
         </v-card>
       </v-dialog>
       <discount-dialog
-        :discount-model-show="discountModelShow"
-        :id="id"
-        :initial-u-i="initialUI"
-        ref="discount"
-        @visibility-changed="(val)=>this.discountModelShow=val"
+          :discount-model-show="discountModelShow"
+          :id="id"
+          :initial-u-i="initialUI"
+          ref="discount"
+          @visibility-changed="(val)=>this.discountModelShow=val"
       />
       <ModificationDrawer
-        ref="modification"
-        @visibility-changed="changeModification"
-        :modification-show="modificationShow"
-        :dish="dish"
-        :old-mod="oldMod"
-        :mod="submitModification"
-        :password="password"
+          ref="modification"
+          @visibility-changed="changeModification"
+          :modification-show="modificationShow"
+          :dish="dish"
+          :old-mod="oldMod"
+          :mod="submitModification"
+          :password="password"
       />
       <check-out-drawer
-        @visibility-changed="changeCheckOut"
-        :order="checkOutModel"
-        :check-out-type="checkOutType"
-        :table-id="id"
-        :password="password"
-        :discount-str="discountStr"
-        :discount-ratio="discountRatio"
-        :visible="checkoutShow"/>
+          @visibility-changed="changeCheckOut"
+          :order="checkOutModel"
+          :check-out-type="checkOutType"
+          :table-id="id"
+          :password="password"
+          :discount-str="discountStr"
+          :discount-ratio="discountRatio"
+          :visible="checkoutShow"/>
     </template>
   </v-app>
 </template>
@@ -427,7 +427,8 @@ left: 304px"
 
 import {
   blocking,
-  blockReady, consumeTypeList,
+  blockReady,
+  consumeTypeList,
   fastSweetAlertRequest,
   findConsumeTypeById,
   getConsumeTypeList,
@@ -451,7 +452,9 @@ import {
   deleteDishes,
   dishesChangeTable,
   dishesSetDiscount,
-  getColorLightness, popChangeTablePanel, popMergeTablePanel,
+  getColorLightness,
+  popChangeTablePanel,
+  popMergeTablePanel,
   printZwichenBon
 } from '@/oldjs/api'
 import { dragscroll } from 'vue-dragscroll'
@@ -484,12 +487,12 @@ const defaultCurrentDish = {
 }
 const keyboardLayout =
 
-  [
-    '7', '8', '9', 'mdi-autorenew',
-    '4', '5', '6', 'A',
-    '1', '2', '3', 'M',
-    '.', '0', 'mdi-close', 'OK'
-  ]
+    [
+      '7', '8', '9', 'mdi-autorenew',
+      '4', '5', '6', 'A',
+      '1', '2', '3', 'M',
+      '.', '0', 'mdi-close', 'OK'
+    ]
 
 // endregion
 export default {
@@ -1196,9 +1199,10 @@ export default {
       this.activeDCT = index
     },
     updateFilteredDish () {
-      this.debounce(() => {
+      if (this.activeCategoryId || this.displayInput) {
+        console.log('should update Filtered Dish')
         this.filteredDish = this.filterDish()
-      })
+      }
     },
     debounce: debounce((f) => {
       f()
@@ -1310,7 +1314,9 @@ export default {
       this.updateFilteredDish()
     },
     input: function () {
-      this.updateFilteredDish()
+      this.debounce(
+        this.updateFilteredDish
+      )
     },
     refresh: function () {
       this.realInitial()
