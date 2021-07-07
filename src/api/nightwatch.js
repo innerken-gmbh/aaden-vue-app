1be556ab58f74ab11cc6887a3217b149be41e97a
Haodong Ju <juhaodong@gmail.com>
2021-07-07 16:48:40 +0200

    引入了全新的更新机制，给予了更加充分的引导，并且支持了Beta版本的更新

--- src/api/nightwatch.js
+++ src/api/nightwatch.js
@@ -1,6 +1,7 @@
 import hillo from 'hillo'
 import GlobalConfig from '@/oldjs/LocalGlobalSettings'
 import IKUtils from 'innerken-js-utils'
+import Swal from 'sweetalert2'
 
 export function checkVersion (version, target) {
   const [main, sub, patch] = version.split('.').map(c => parseInt(c))
@@ -11,14 +12,33 @@ export function checkVersion (version, target) {
 }
 
 export async function update () {
-  setTimeout(() => {
-    IKUtils.hideLoading(0)
-  }
-  , 30000)
+  let timerInterval
+  Swal.fire({
+    title: 'Automatical Update',
+    html: 'Muss noch <b></b> Sekunden warten.',
+    timer: 30000,
+    timerProgressBar: true,
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading()
+      timerInterval = setInterval(() => {
+        const content = Swal.getHtmlContainer()
+        if (content) {
+          const b = content.querySelector('b')
+          if (b) {
+            b.textContent = Swal.getTimerLeft()
+          }
+        }
+      }, 1000)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  })
   await hillo.get('MyVersion.php?op=update')
   IKUtils.toast('update ok')
   if (!await checkCurrentVersion()) {
-    IKUtils.showError('Automatisch Update felher.', 'Bitte update Sie manual')
+    IKUtils.showError('Automatisch Update felher.', ' Bitte Aaden Support kontakt und Upgrade')
   }
 }
 
@@ -29,10 +49,18 @@ export async function getCurrentBackendVersion () {
 }
 
 export async function checkCurrentVersionAndUpdate () {
-  const versionOk = await checkCurrentVersion(false)
+  const versionOk = await checkCurrentVersion(true)
   if (!versionOk) {
-    IKUtils.showLoading(false)
-    update()
+    const result = await Swal.fire({
+      title: 'Update Jetzt?',
+      icon: 'warning',
+      text: 'Deine Backend Version ist zu alt.',
+      showConfirmButton: true,
+      showCancelButton: true
+    })
+    if (result.isConfirmed) {
+      await update()
+    }
   }
 }
 
