1340558daeb756b37196e0721b1c7bde7bd06883
icydresden <icydresden@gmail.com>
2021-07-15 17:02:10 +0200

    Commit修正

--- src/components/fragments/UpdateFragment.vue
+++ src/components/fragments/UpdateFragment.vue
@@ -5,10 +5,7 @@
         {{ $t('Achtung! Bitte Upgrade') }}
       </v-card-title>
       <v-card-text v-if="!fold">
-        1. 更新根据店里的网络情况，将会需要1-10分钟。<br>
-        2. 更新只能在有网络的情况下进行。<br>
-        3. 更新成功后，建议重新启动应用。重新启动应用的方法是：按一下键盘上的Windows键（四个方块），然后点红色叉叉关掉应用，然后重新打开。<br>
-        4. 如果提示更新失败，请尽快联系我们进行手动更新。
+        {{ $t('AchtungText') }}
       </v-card-text>
       <v-btn large
              @click="fold=!fold"
