01fafc93c60cb0d7a739c6efb3c745fff98fa9a6
IcyDresden <icydresden@gmail.com>
2021-12-08 10:45:31 +0100

    修正MilePay的IP本地化问题

--- src/oldjs/LocalGlobalSettings.js
+++ src/oldjs/LocalGlobalSettings.js
@@ -13,7 +13,8 @@ export async function loadConfig () {
     changeLanguage(GlobalConfig.lang)
     NeededKeys = GlobalConfig.neededKeys
     GlobalConfig.getMilepayUrl = function () {
-      return GlobalConfig.Protocol + '192.168.1.100' + ':' + GlobalConfig.milePayPort + '/milePay/'
+      // return GlobalConfig.Protocol + '192.168.1.100' + ':' + GlobalConfig.milePayPort + '/milePay/'
+      return GlobalConfig.Protocol + GlobalConfig.base + ':' + GlobalConfig.milePayPort + '/milePay/'
     }
     window.Config = GlobalConfig
   } catch (e) {
