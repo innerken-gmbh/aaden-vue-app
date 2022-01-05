7901d43a9ba29134978597ac9cffbf891b701ec9
CI BOT InnerKen <admin@innerken.com>
2022-01-05 08:54:59 +0000

    AUTO USE LATEST EMBEDED BUILD

--- public/admin/service-worker.js
+++ public/admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.0e3029411b7bb30744e347b449380623.js"
+  "precache-manifest.8b464099ea9571a644d4d9d4f62b8330.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aaden-admin"});
