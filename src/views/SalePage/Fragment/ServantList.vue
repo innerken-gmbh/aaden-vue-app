6d4fd0bd3192c0810217369ce369acb0dbe684d2
qi1n <44217604+qi1n@users.noreply.github.com>
2022-04-22 11:44:37 +0200

    样式上的小调整

--- src/views/SalePage/Fragment/ServantList.vue
+++ src/views/SalePage/Fragment/ServantList.vue
@@ -1,99 +1,102 @@
 <template>
-  <div class="px-4" style="max-height: calc(100vh - 112px); overflow-y: scroll">
-    <v-subheader class="my-4">
-      <v-spacer></v-spacer>
-      <v-btn x-large dark class="orange mr-2 mb-6" style="position: absolute; right: 0" @click="allZBon(...singleZBonDate)">打印全部跑堂日结单</v-btn>
-    </v-subheader>
-    <div
-      style="overflow: scroll; "
-      class="mt-2 mx-4">
-      <div style="display: grid; grid-template-columns: repeat(3,1fr); grid-gap: 20px; grid-auto-rows: 305px;min-height: calc(90vh - 120px)">
-        <div
-          v-for="s in displayServantInfo"
-          :key="s.servant.id"
-          style="background: #f0f0f0;overflow: visible; border-radius: 16px; "
-          :style="activeId===s.servant.id ? {zIndex: 100}:{}"
-          @click="changeActiveId(s.servant.id)"
-        >
+  <div style="max-height: calc(100vh - 112px); overflow-y: scroll">
+    <div class="d-flex">
+      <div
+        style="min-width: calc(100% - 272px)"
+        class="mt-2 mx-4">
+        <div style="display: grid; grid-template-columns: repeat(3,1fr); grid-gap: 20px; grid-auto-rows: 305px;min-height: calc(90vh - 120px)">
           <div
-            style="background: #f0f0f0;"
-            class="pa-2"
+            v-for="s in displayServantInfo"
+            :key="s.servant.id"
+            style="background: #f0f0f0;overflow: visible; border-radius: 16px; "
+            :style="activeId===s.servant.id ? {zIndex: 100}:{}"
+            @click="changeActiveId(s.servant.id)"
           >
-            <div class="d-flex justify-space-between align-center py-2">
-              <div class="display-1">
-                {{ s.servant.name }}
-              </div>
-              <v-btn
-                class="mr-0"
-                small
-                color="warning"
-                @click="singleZBon(s.servant.password)"
-              >
-                {{ $t('打印日结单') }}
-              </v-btn>
-            </div>
-
-            <v-divider></v-divider>
-            <div>
-              <div class="py-2">
-                <div
-                  v-for="pay in fillPayMethodTotal(s.payMethodTotal,activeId!==s.servant.id)"
-                  :key="pay.id"
-                  class="d-flex justify-space-between pt-1"
-                >
-                  <div class="label">
-                    {{ pay.name }}
-                  </div>
-                  <div class="value font-weight-bold">
-                    {{ pay.amount | priceDisplay }}
-                  </div>
+            <div
+              style="background: #f0f0f0;"
+              class="pa-2"
+            >
+              <div class="d-flex justify-space-between align-center py-2">
+                <div class="display-1">
+                  {{ s.servant.name }}
                 </div>
-
-                <div
-                  class="d-flex justify-start py-1"
+                <v-btn
+                  class="mr-0"
+                  small
+                  color="warning"
+                  @click="singleZBon(s.servant.password)"
                 >
-                  <div
-                    v-if="activeId!==s.servant.id"
-                    style="border-bottom: 1px solid #4D8AED;color: #4D8AED"
-                  >
-                    {{ $t('展示更多') }}
-                  </div>
-                  <div
-                    v-else
-                    style="border-bottom: 1px solid #4D8AED;color: #4D8AED"
-                  >
-                    {{ $t('收起') }}
-                  </div>
-                </div>
+                  {{ $t('打印日结单') }}
+                </v-btn>
               </div>
 
               <v-divider></v-divider>
+              <div>
+                <div class="py-2">
+                  <div
+                    v-for="pay in fillPayMethodTotal(s.payMethodTotal,activeId!==s.servant.id)"
+                    :key="pay.id"
+                    class="d-flex justify-space-between pt-1"
+                  >
+                    <div class="label">
+                      {{ pay.name }}
+                    </div>
+                    <div class="value font-weight-bold">
+                      {{ pay.amount | priceDisplay }}
+                    </div>
+                  </div>
 
-              <div
-                class="d-flex justify-space-between pt-1"
-              >
-                <div class="label">
-                  {{ $t('小费') }}
+                  <div
+                    class="d-flex justify-start py-1"
+                  >
+                    <div
+                      v-if="activeId!==s.servant.id"
+                      style="border-bottom: 1px solid #4D8AED;color: #4D8AED"
+                    >
+                      {{ $t('展示更多') }}
+                    </div>
+                    <div
+                      v-else
+                      style="border-bottom: 1px solid #4D8AED;color: #4D8AED"
+                    >
+                      {{ $t('收起') }}
+                    </div>
+                  </div>
                 </div>
-                <div class="value font-weight-bold">
-                  {{ s.tipIncome ? s.tipIncome : 0 | priceDisplay }}
-                </div>
-              </div>
 
-              <div
-                class="d-flex justify-space-between pt-1 "
-              >
-                <div class="label">
-                  {{ $t('今日总计(含小费)') }}
+                <v-divider></v-divider>
+
+                <div
+                  class="d-flex justify-space-between pt-1"
+                >
+                  <div class="label">
+                    {{ $t('小费') }}
+                  </div>
+                  <div class="value font-weight-bold">
+                    {{ s.tipIncome ? s.tipIncome : 0 | priceDisplay }}
+                  </div>
                 </div>
-                <div class="value font-weight-bold">
-                  {{ s.todayTotal ? s.todayTotal : 0 | priceDisplay }}
+
+                <div
+                  class="d-flex justify-space-between pt-1 "
+                >
+                  <div class="label">
+                    {{ $t('今日总计(含小费)') }}
+                  </div>
+                  <div class="value font-weight-bold">
+                    {{ s.todayTotal ? s.todayTotal : 0 | priceDisplay }}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <v-navigation-drawer permanent right width="272">
+        <v-card elevation="0" class="pl-6">
+          <v-btn right x-large dark class="orange mb-6 mt-2" block @click="allZBon(...singleZBonDate)">打印全部跑堂日结单</v-btn>
+        </v-card>
+      </v-navigation-drawer>
     </div>
   </div>
 </template>
