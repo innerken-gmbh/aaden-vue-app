6b0a90fbd1dea95f036c22a64a5d150328658810
Haodong Ju <juhaodong@gmail.com>
2022-09-23 20:00:13 +0200

    [VersionChange]:: release v0.10.7

--- src/background.js
+++ src/background.js
@@ -20,13 +20,18 @@ protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: tru
 
 function createWindow () {
   // Create the browser window.
+  const splash = new BrowserWindow({
+    width: 500, height: 300, transparent: false, frame: false, alwaysOnTop: true
+  })
+  createProtocol('app')
+  splash.loadFile('app//./splash.html')
+  splash.center()
   win = new BrowserWindow({
     width: 1920,
     height: 1080,
     show: false,
     webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
+      nodeIntegration: true, enableRemoteModule: true
     }
   })
   const Debug = settings.getSync('config.Debug')
@@ -36,15 +41,17 @@ function createWindow () {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
   } else {
-    createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
   win.once('ready-to-show', () => {
-    win.show()
-    if (Debug) {
-      win.webContents.openDevTools()
-    }
+    setTimeout(() => {
+      splash.close()
+      win.show()
+      if (Debug) {
+        win.webContents.openDevTools()
+      }
+    }, 3000)
   })
 
   win.on('closed', () => {
