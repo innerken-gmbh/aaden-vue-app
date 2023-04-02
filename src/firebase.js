fc6aa8545bc5bcd71530fba501c08e7b25d89e6a
Haodong Ju <juhaodong@gmail.com>
2023-04-02 09:54:36 +0200

    [VersionChange]:: release v0.10.52

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { doc, updateDoc } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 
 import { db } from '@/api/fireStore'
 
@@ -8,15 +8,15 @@ export const FirestorageDB = db
 
 export async function setOrderListInFirebase (orderList, deviceIdPath) {
   const data = { order: orderList }
-  await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
 }
 
 export async function setCartListInFirebase (cartList, deviceIdPath) {
   const data = { cart: cartList }
-  await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
 }
 
 export async function setCheckOutStatusInFirebase (showBetrag, deviceIdPath) {
   const data = { showBetrag: showBetrag }
-  await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
 }
