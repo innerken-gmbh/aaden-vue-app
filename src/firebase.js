40465a83c14544b05086ec355f656c5f37010f03
Haodong Ju <juhaodong@gmail.com>
2023-04-02 22:59:20 +0200

    试试看能不能解决问题

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,27 @@ export const FirestorageDB = db
 
 export async function setOrderListInFirebase (orderList, deviceIdPath) {
   const data = { order: orderList }
-  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  try {
+    await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export async function setCartListInFirebase (cartList, deviceIdPath) {
   const data = { cart: cartList }
-  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  try {
+    await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export async function setCheckOutStatusInFirebase (showBetrag, deviceIdPath) {
   const data = { showBetrag: showBetrag }
-  await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  try {
+    await setDoc(doc(FirestorageDB, firebasePath, deviceIdPath), data)
+  } catch (e) {
+    console.log(e)
+  }
 }
