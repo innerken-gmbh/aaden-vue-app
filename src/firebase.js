efd5edc04d847c38f1a18b03c2fede1406045be7
Haodong Ju <25146689+juhaodong@users.noreply.github.com>
2023-05-18 09:54:32 +0200

    [VersionChange]:: release v0.10.58

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,16 @@
 import { doc, updateDoc } from 'firebase/firestore'
 
 import { db } from '@/api/fireStore'
+import GlobalConfig from '@/oldjs/LocalGlobalSettings'
 
 const firebasePath = 'customerDisplay'
 
 export const FirestorageDB = db
 
 export async function setOrderListInFirebase (orderList, deviceIdPath) {
+  if (!GlobalConfig.useCustomerDisplay) {
+    return
+  }
   const data = { order: orderList }
   try {
     await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath.toString()), data)
@@ -16,6 +20,9 @@ export async function setOrderListInFirebase (orderList, deviceIdPath) {
 }
 
 export async function setCartListInFirebase (cartList, deviceIdPath) {
+  if (!GlobalConfig.useCustomerDisplay) {
+    return
+  }
   const data = { cart: cartList }
   try {
     await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath.toString()), data)
@@ -25,6 +32,9 @@ export async function setCartListInFirebase (cartList, deviceIdPath) {
 }
 
 export async function setCheckOutStatusInFirebase (showBetrag, deviceIdPath) {
+  if (!GlobalConfig.useCustomerDisplay) {
+    return
+  }
   const data = { showBetrag: showBetrag }
   try {
     await updateDoc(doc(FirestorageDB, firebasePath, deviceIdPath.toString()), data)
