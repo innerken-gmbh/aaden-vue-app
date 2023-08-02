c2b03349ef19f63865de336eaa87a307380cea90
Haodong Ju <25146689+juhaodong@users.noreply.github.com>
2023-08-02 14:44:20 +0200

    加入DeviceId设置

--- src/views/BossPage.vue
+++ src/views/BossPage.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe
       width="100%"
-      :src="'http://'+Config.Base+'/'+endPoint+'?Base='+Config.Base
+      :src="'http://'+Config.Base+'/'+endPoint+'?BaseId='+deviceId
           +'&lang='+Config.lang+'&chaos='+Config.startUpTimestamp+'&pw='+password"
       style="height: 100vh"
   ></iframe>
@@ -9,17 +9,22 @@
 
 <script>
 import GlobalConfig from '@/oldjs/LocalGlobalSettings'
+import { getCurrentDeviceId } from '@/api/VIPCard/VIPCloudApi'
 
 export default {
   name: 'BossPage',
   data: function () {
     return {
       Config: GlobalConfig,
-      endPoint: 'Admin-Edge'
+      endPoint: 'Admin-Edge',
+      deviceId: null
     }
   },
   props: {
     password: {}
+  },
+  async mounted () {
+    this.deviceId = await getCurrentDeviceId()
   }
 }
 </script>
