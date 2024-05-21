d84c97ec97236411ea3817d38ab9a8ed398df2eb
Haodong Ju <juhaodong@gmail.com>
2024-05-21 18:06:09 +0200

    [VersionChange]:: release v1.2.5

--- vue.config.js
+++ vue.config.js
@@ -18,6 +18,7 @@ module.exports = {
           publish: [
             {
               provider: 'snapStore',
+              repo: 'aaden',
               channels: 'stable'
             }
           ]
